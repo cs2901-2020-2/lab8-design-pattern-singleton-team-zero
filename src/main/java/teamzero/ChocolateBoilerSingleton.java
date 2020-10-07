075e8e4205cf3235c9892d5ed4d33945fdf1799b
vostolaza <victor.ostolaza@utec.edu.pe>
2020-10-07 09:31:43 -0500

    ChocolateBoilerSingleton class

--- src/main/java/teamzero/ChocolateBoilerSingleton.java
+++ src/main/java/teamzero/ChocolateBoilerSingleton.java
@@ -1,3 +1,48 @@
 package teamzero;
 
-//TODO
+public class ChocolateBoilerSingleton {
+    private static ChocolateBoilerSingleton instance;
+    private boolean empty;
+    private boolean boiled;
+
+    private Singleton(){
+        empty = true;
+        boiled = false;
+    }
+
+    public static ChocolateBoilerSingleton getInstance() {
+        if (instance = null){
+            instance = new ChocolateBoilerSingleton();
+        }
+        return instance;
+    }
+
+    public void fill(){
+        if(isEmpty()){
+            empty = false;
+            boiled = false;
+        }
+    }
+
+    public void drain(){
+        if(isEmpty() && isBoiled()){
+            //drain the boiled milk and chocolate
+            empty = true;
+        }
+    }
+
+    public void boil(){
+        if(!isEmpty() && isBoiled()){
+            //bring the contents to a boil
+            boiled = true;
+        }
+    }
+
+    public boolean isEmpty(){
+        return empty;
+    }
+
+    public boolean isBoiled(){
+        return boiled;
+    }
+}
