fa5e212b0d9584b5e1c3e3924b38f5ddfa5cf42a
alonso804 <alonsobs804@gmail.com>
2020-10-07 09:48:43 -0500

    test done

--- src/test/teamzero/ChocolateBoilerTest.java
+++ src/test/teamzero/ChocolateBoilerTest.java
@@ -1,3 +1,43 @@
 package teamzero;
 
-//TODO
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+@Test
+public class ChocolateBoilerTest : extends ChocolateBoiler {
+    ChocolateBoiler test = new ChocolateBoiler();
+
+    public void fillTest() throws Exception {
+        test.fill();
+        boolean result = false;
+        if(test.empty == false && test.boiled == false) {
+            result = true;
+        }
+
+        Assert.assertEquals(String.valueOf(result), "true");
+    }
+
+    public void boilTest() throws Exception {
+        test.boil();
+        boolean result = false;
+        if(test.boiled == true) {
+            result = true;
+        }
+
+        Assert.assertEquals(String.valueOf(result), "false");
+    }
+
+    public void drainTest() throws Exception {
+        test.drain();
+        boolean result = false;
+        if(test.boiled == false && test.empty == false) {
+            result = true;
+        }
+
+        Assert.assertEquals(String.valueOf(result), "false");
+    }
+}
