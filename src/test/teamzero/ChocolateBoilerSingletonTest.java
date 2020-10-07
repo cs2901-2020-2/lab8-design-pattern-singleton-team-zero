40dd3a9da875f8008d97ddf9b11d2353dcc2b10d
vostolaza <victor.ostolaza@utec.edu.pe>
2020-10-07 09:58:29 -0500

    SingletonTest structure

--- src/test/teamzero/ChocolateBoilerSingletonTest.java
+++ src/test/teamzero/ChocolateBoilerSingletonTest.java
@@ -1,3 +1,36 @@
 package teamzero;
 
-//TODO
+@Test
+public class ChocolateBoilerSingletonTest {
+    ChocolateBoilerSingleton test = ChocolateBoilerSingleton.getInstance();
+
+    public void fillTest() throws Exception {
+        test.fill();
+        boolean result = false;
+        if(test.isEmpty() == false && test.isBoiled() == false) {
+            result = true;
+        }
+
+        Assert.assertEquals(String.valueOf(result), "true");
+    }
+
+    public void boilTest() throws Exception {
+        test.boil();
+        boolean result = false;
+        if(test.isBoiled() == true) {
+            result = true;
+        }
+
+        Assert.assertEquals(String.valueOf(result), "false");
+    }
+
+    public void drainTest() throws Exception {
+        test.drain();
+        boolean result = false;
+        if(test.isBoiled() == false && test.isEmpty() == false) {
+            result = true;
+        }
+
+        Assert.assertEquals(String.valueOf(result), "false");
+    }
+}
